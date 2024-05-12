ea407effb0431dba620cd7a03956379a3467ff7e
NUO0203 <134362730+NUO0203@users.noreply.github.com>
2024-05-12 14:36:47 +0800

    commands accepted

--- CSOPESY.cpp
+++ CSOPESY.cpp
@@ -13,38 +13,71 @@ void printHeader() {
 	std::cout << "| <__ \\__ \\ | | | |  _/ | _>  \\__ \\ \\   /\n";
 	std::cout << "`___/ <___/ `___' |_|   |___> <___/  |_|\n";
 	std::cout << GREEN << "Hello, Welcome to CSOPESY commandline!\n" << DEFAULT;
-	std::cout << "Type '" << RED << "exit" << DEFAULT << "' to quit, '" << RED  << "clear" << DEFAULT << "' to clear the screen\n";
+	std::cout << "Type '" << RED << "exit" << DEFAULT << "' to quit, '" << RED << "clear" << DEFAULT << "' to clear the screen\n";
 }
 
 //command function templates
-void clear(){
-    //clear screen
-	system(cls); 
+void clear() {
+	// clear screen
+	system("cls");
+    printHeader();
 }
 
 void marquee() {
 	//doing something
+    std::cout << "marquee command recognized. Doing something \n";
 }
 
+void screen() {
+    //doing something
+    std::cout << "screen command recognized. Doing something \n";
+}
+
+void processsmi() {
+    //doing something
+    std::cout << "process-smi command recognized. Doing something \n";
+}
+
+void nvidiasmi() {
+    //doing something
+    std::cout << "nvidia-smi command recognized. Doing something \n";
+}
 
 int main()
 {
-	std::string command; 
+    std::string command;
 
-	//while loop until user exits
-	printHeader();
+    printHeader();
 
-	//input
-	std::cout << "Enter Command: ";
-	std::cin >> command;
+    //while loop until user exits
+    while (true) {
 
+        //input
+        std::cout << "Enter Command: ";
+        std::cin >> command;
 
-	if (command == "clear") {
-		clear();
-		printHeader();
-	}
-	else {
-		std::cout << "efjpwfofjpejf";
-	}
-	
+        if (command == "exit") {
+            break; // Exit the loop and end the program
+        }
+        else if (command == "clear") {
+            clear();
+        }
+        else if (command == "marquee") {
+            marquee();
+        }
+        else if (command == "screen") {
+            screen();
+        }
+        else if (command == "process-smi") {
+            processsmi();
+        }
+        else if (command == "nvidia-smi") {
+            nvidiasmi();
+        }
+        else {
+            std::cout << "Invalid command. Please try again.\n";
+        }
+    }
+
+    return 0;
 }
